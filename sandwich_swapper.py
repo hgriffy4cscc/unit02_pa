df589237a625fb0f46eaab4a4fa86a290cb028cc
Henry <hgriffy@student.cscc.edu>
2026-06-11 07:51:06 -0400

    partway through

---
+++ sandwich_swapper.py
@@ -0,0 +1,8 @@
+"""7-8. Deli: Make a list called sandwich_orders and fill it with the names of various sandwiches. Then make an empty list called finished_sandwiches. Loop through the list of sandwich orders and print a message for each order, such as I made your tuna sandwich. As each sandwich is made, move it to the list of finished sandwiches. After all the sandwiches have been made, print a message listing each sandwich that was made."""
+
+sandwich_orders : str = ["veggie","avocado toast","falafel","taco"]
+finished_sandwiches : str = []
+
+for each sandwich in sandwich_orders:
+    print(f{"I made your {sandwich} sandwich!"})
+    
